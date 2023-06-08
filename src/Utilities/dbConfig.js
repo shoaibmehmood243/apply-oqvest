ae940a4c8b6e698f0cba5b6d152117e86980a4cb
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-08 17:26:17 +0500

    Vercel configuration setup

--- src/Utilities/dbConfig.js
+++ src/Utilities/dbConfig.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
+// const db = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'apply_oqvest'
+// });
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'apply_oqvest'
+    host: '191.101.13.52',
+    user: 'u116608330_apply_oqvest',
+    password: 'Temppass@22',
+    database: 'u116608330_apply_oqvest'
 });
-
 db.getConnection((err, connection)=> {
     if(err) {
         console.log(err);
