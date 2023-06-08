d1c994fbe4cfa4d5879c7323fdf8271594065d7b
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-08 17:32:08 +0500

    Vercel configuration setup

--- src/Models/clients.model.js
+++ src/Models/clients.model.js
@@ -1,5 +1,5 @@
 const db = require("../Utilities/dbConfig");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 class Clients {
     first_name;
--- src/Utilities/bcrypt.js
+++ src/Utilities/bcrypt.js
@@ -1,15 +1,15 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const comparePassword = async (provided, current, cb) => {
-    try {
-      const result = await bcrypt.compare(provided, current);
-      if (!result) {
-        throw "Invalid email/password";
-      }
-      cb(undefined, result);
-    } catch (err) {
-      cb(err, undefined)
+  try {
+    const result = await bcrypt.compare(provided, current);
+    if (!result) {
+      throw "Invalid email/password";
     }
+    cb(undefined, result);
+  } catch (err) {
+    cb(err, undefined)
+  }
 };
 
-module.exports = {comparePassword};
+module.exports = { comparePassword };
