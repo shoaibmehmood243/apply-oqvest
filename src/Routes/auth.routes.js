e1ec96d61ab466663e5af849209e6ab5f10aa75a
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-08 16:25:50 +0500

    worked on forget password and reset password

--- src/Routes/auth.routes.js
+++ src/Routes/auth.routes.js
@@ -4,6 +4,6 @@ const authController = require('../Controllers/auth.controller')
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/forget-password', authController.forgetPassword);
-router.post('/set-password/:token', authController.setPassword);
+router.post('/reset-password/:token', authController.setPassword);
 
 module.exports = router;
