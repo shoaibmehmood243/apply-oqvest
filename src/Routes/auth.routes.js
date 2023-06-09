d27ef5aa25d21abef20123a9e1a24f7aff108a67
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-09 15:18:47 +0500

    client app created

--- src/Routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require('../Controllers/auth.controller')
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/forget-password', authController.forgetPassword);
-router.post('/reset-password/:token', authController.setPassword);
-
-module.exports = router;
