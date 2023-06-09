8f521f3356314a7433ecf8ffadcfa408cd1d5268
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-06-09 16:08:12 +0500

    client libraries setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
